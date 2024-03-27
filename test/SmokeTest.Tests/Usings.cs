250238150a377516c4ecd38450e8e0c69ae53052
Philip Conrad <conradp@chariot-chaser.net>
2024-03-27 16:55:10 -0400

    ci: Add basic Pull Request checks. (#5)
    * ci: Add basic Pull Request checks.
    
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>
    
    * test: Add first smoke test for policy eval.
    
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>
    
    * ci: Add Smoke Test job.
    
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>
    
    ---------
    
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>

---
+++ test/SmokeTest.Tests/Usings.cs
@@ -0,0 +1,6 @@
+global using Xunit;
+global using DotNet.Testcontainers.Builders;
+global using DotNet.Testcontainers.Containers;
+global using DotNet.Testcontainers.Images;
+global using DotNet.Testcontainers.Networks;
+global using Newtonsoft.Json;
