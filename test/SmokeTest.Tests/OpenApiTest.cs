66a736b9f828a4eb552e359180e6bbe534de17c4
Philip Conrad <philipaconrad@gmail.com>
2024-05-21 18:02:58 -0400

    test: Add DefaultPolicy tests.
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>

--- test/SmokeTest.Tests/OpenApiTest.cs
+++ test/SmokeTest.Tests/OpenApiTest.cs
@@ -67,4 +67,39 @@ public class OpenApiTest : IClassFixture<OPAContainerFixture>
 
     Assert.Equal(true, resultMap?.GetValueOrDefault("allow", false));
   }
+
+  [Fact]
+  public async Task OpenApiClientEvaluateDefaultTest()
+  {
+    // Construct the request URI by specifying the scheme, hostname, assigned random host port, and the endpoint "uuid".
+    var requestUri = new UriBuilder(Uri.UriSchemeHttp, _container.Hostname, _container.GetMappedPublicPort(8181)).Uri;
+
+    // Send an HTTP GET request to the specified URI and retrieve the response as a string.
+    var client = new OpaApiClient(serverIndex: 0, serverUrl: requestUri.ToString());
+
+    // Exercise the low-level OPA C# SDK.
+    ExecuteDefaultPolicyWithInputRequest req = new ExecuteDefaultPolicyWithInputRequest()
+    {
+      Input = Input.CreateMapOfany(
+                    new Dictionary<string, object>() {
+                    { "user", "alice" },
+                    { "action", "read" },
+                    { "object", "id123" },
+                    { "type", "dog" },
+                    }),
+    };
+
+    // Note(philip): To to how the API is generated, we have to fire off
+    // requests directly-- there's no building of requests in advance for later
+    // launching.
+    var res = await client.ExecuteDefaultPolicyWithInputAsync(Input.CreateMapOfany(
+                    new Dictionary<string, object>() {
+                    { "hello", "world" },
+                    }));
+    var resultMap = res.Result?.MapOfany;
+
+    // Ensure we got back the expected fields from the eval.
+    Assert.Equal("this is the default path", resultMap?.GetValueOrDefault("msg", ""));
+    Assert.Equal(new Dictionary<string, object>() { { "hello", "world" } }, resultMap?.GetValueOrDefault("echo", ""));
+  }
 }
